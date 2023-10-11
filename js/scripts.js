ddad89e2d51f0887723f74107545599b3d199c13
adrianogomesz <adrianodede76@gmail.com>
2023-10-11 08:44:57 -0300

    logica da condicao de vitoria

--- js/scripts.js
+++ js/scripts.js
@@ -43,6 +43,61 @@ function definirPlayer(player1, player2) {
 }
 
 
+function checkWinCondition() {
+
+    let b1 = document.getElementById('bloco-1');
+    let b2 = document.getElementById('bloco-2');
+    let b3 = document.getElementById('bloco-3');
+    let b4 = document.getElementById('bloco-4');
+    let b5 = document.getElementById('bloco-5');
+    let b6 = document.getElementById('bloco-6');
+    let b7 = document.getElementById('bloco-7');
+    let b8 = document.getElementById('bloco-8');
+    let b9 = document.getElementById('bloco-9');
+
+
+    let b1Child = b1.childNodes[0].className;
+    let b2Child = b2.childNodes[0].className;
+    let b3Child = b3.childNodes[0].className;
+
+    let b4Child = b4.childNodes[0].className;
+    let b5Child = b5.childNodes[0].className;
+    let b6Child = b6.childNodes[0].className;
+
+    let b7Child = b7.childNodes[0].className;
+    let b8Child = b8.childNodes[0].className;
+    let b9Child = b9.childNodes[0].className;
+
+    // condicoes horizontais 
+
+    if(b1Child == 'x' && b2Child == 'x' && b3Child == 'x') {
+        console.log("X venceu");
+
+    } else if(b1Child == 'o' && b2Child == 'o' && b3Child == 'o') {
+        console.log("O venceu");
+
+    }
+
+
+    if(b4Child == 'x' && b5Child == 'x' && b6Child == 'x') {
+        console.log("X venceu");
+
+    } else if(b4Child == 'o' && b5Child == 'o' && b6Child == 'o') {
+        console.log("O venceu");
+
+    }
+
+
+    if(b7Child == 'x' && b8Child == 'x' && b9Child == 'x') {
+        console.log("X venceu");
+
+    } else if(b7Child == 'o' && b8Child == 'o' && b9Child == 'o') {
+        console.log("O venceu");
+
+    }
+}
+
+
 // logica para iniciar o jogo
 
 let player1 = 0;
@@ -63,6 +118,7 @@ for (let i = 0; i < blocos.length; i++) {
 
             // computar a jogada 
 
+
             if (player1 == player2) {
 
                 player1++
@@ -73,8 +129,10 @@ for (let i = 0; i < blocos.length; i++) {
 
             }
 
+            checkWinCondition();
+
         }
+           
     });
 
 }
-
